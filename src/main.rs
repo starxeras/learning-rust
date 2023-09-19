8d740562c3d010c3b4dde8f17b34d5ccd491c846
starxeras <starxeras@gmail.com>
2023-09-18 19:59:03 -0500

    :beers: just random things

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,45 @@ If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 */
 
+extern crate question;
+use rand::prelude::*;
+// use question::{Question, Answer};
+
+pub struct Random;
+
+impl Random {
+    fn hc128_rng(min: i32, max: i32) -> i32 {
+        let mut rng: rand_hc::Hc128Rng = rand_hc::Hc128Rng::from_entropy();
+        return rng.gen_range(min..max);
+    }
+    fn chacha20_rng(min: i32, max: i32) -> i32 {
+        let mut rng: rand_chacha::ChaCha20Rng = rand_chacha::ChaCha20Rng::from_entropy();
+        return rng.gen_range(min..max);
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    loop {
+        
+        let min = Random::hc128_rng(1, 1000);
+        let max = Random::hc128_rng(1, 1000);
+
+        if max <= min {
+            continue;
+        }
+
+        let mut count = Random::hc128_rng(min, max);
+        while count <= 1000 {
+            println!("Current count: {}", count);
+            println!("---------------");
+            println!("Hc128Rng: {}", Random::hc128_rng(min ,max));
+            println!("Chacha20Rng: {}\n-----------------", Random::chacha20_rng(min ,max));
+            count += 1;
+        }
+
+    }
+
 }
